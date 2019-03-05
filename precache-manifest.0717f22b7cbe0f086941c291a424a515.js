59732ead7bfc1f439cd9f927ff34d1baa8becb26
nikolaevskiy.m <mik639inc@gmail.com>
2019-03-06 00:39:02 +0400

    Updates

---
+++ precache-manifest.0717f22b7cbe0f086941c291a424a515.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d13f9d84404dccde6d",
+    "url": "/redux-cart/static/js/runtime~main.84d13f9d.js"
+  },
+  {
+    "revision": "4a0a8362404cd95e69f7",
+    "url": "/redux-cart/static/js/main.4a0a8362.chunk.js"
+  },
+  {
+    "revision": "93b1f555e37e5384b4f9",
+    "url": "/redux-cart/static/js/2.93b1f555.chunk.js"
+  },
+  {
+    "revision": "4a0a8362404cd95e69f7",
+    "url": "/redux-cart/static/css/main.9c7757c5.chunk.css"
+  },
+  {
+    "revision": "9de10e7289331735812f48bc778ae0a0",
+    "url": "/redux-cart/index.html"
+  }
+];
